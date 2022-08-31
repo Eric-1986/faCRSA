26cc633fc3ce33737f8e6760d5e413f657365f2c
njauzrn <2020801253@stu.njau.edu.cn>
2022-08-31 11:55:19 +0800

    fix several bugs

--- tasks.py
+++ tasks.py
@@ -7,13 +7,24 @@ LastEditTime: 2022-07-01 12:05:33
 E-mail: 2020801253@stu.njau.edu.cn
 Copyright (c) 2022 by Ruinan Zhang, All Rights Reserved. Licensed under the GPL v3.0.
 '''
-from task_queue import huey
+from huey import CancelExecution
+from huey.signals import SIGNAL_ERROR
 
+from task_queue import huey
 
 @huey.task()
 def submit_task(uid, tid):
-    web_action(uid, tid)
-    return tid
+    try:
+        web_action(uid, tid)
+    except (PermissionError, FileExistsError, IndexError, KeyError, NameError) as e:
+        raise CancelExecution()
+
+@huey.signal()
+def print_signal_args(signal, task, exc=None):
+    if signal == SIGNAL_ERROR:
+        print('%s - %s - exception: %s' % (signal, task.id, exc))
+    else:
+        print('%s - %s' % (signal, task.id))
 
 
 from facrsa_code.library.analysis.main import web_action
